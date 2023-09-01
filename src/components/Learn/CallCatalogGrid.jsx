dd1e6a4301a6de3dbdd0b1e7c33cb31c44519540
Paul Cretu <paul.s.cretu@gmail.com>
2023-09-01 18:52:30 +0000

    Fix catalog grid sounds

--- src/components/Learn/CallCatalogGrid.jsx
+++ src/components/Learn/CallCatalogGrid.jsx
@@ -12,7 +12,14 @@ import SO6 from '../../../public/audio/FO-S06.mp3'
 import OrcaSound from '../../../public/images/learn/orcasound.png'
 
 const CallCatalogGrid = () => {
-  const [play] = useSound([SO1, SO2, SO3, SO4, SO5, SO6])
+  const [playS01] = useSound(SO1)
+  const [playS02] = useSound(SO2)
+  const [playS03] = useSound(SO3)
+  const [playS04] = useSound(SO4)
+  const [playS05] = useSound(SO5)
+  const [playS06] = useSound(SO6)
+
+  const playArray = [playS01, playS02, playS03, playS04, playS05, playS06]
 
   return (
     <div>
@@ -31,7 +38,7 @@ const CallCatalogGrid = () => {
           spacing={{ xs: 2, md: 12 }}
           columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          {Array.from(Array(6)).map((_, index) => (
+          {playArray.map((play, index) => (
             <Grid item xs={2} sm={4} md={4} key={index}>
               <Image
                 src={OrcaSound}
